6ecbe4d949de982e8d660fbae2dd046e8eb8011a
Ryan Olea <ryanaolea@gmail.com>
2021-12-25 12:50:58 -0800

    update styles to use unicode

--- client/src/components/RatingsComponents/ReviewBody.jsx
+++ client/src/components/RatingsComponents/ReviewBody.jsx
@@ -4,7 +4,7 @@ function ReviewBody () {
 
   return (
     <div className='body-tile'>
-      <p>This is the Review Summary</p>
+      <p>This is the Review Summary ☑ </p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Senectus et netus et malesuada fames. In tellus integer feugiat scelerisque varius morbi.</p>
     </div>
   )
