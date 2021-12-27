dab57ff2bdde5d1b05f2cd6334019610095566ee
Wisdom Ibole <wisdom.ibole@gmail.com>
2021-12-26 21:23:03 -0800

    Adds config.js file for GitHub Token; adds helper functions for API GET, POST, and PUT requests

---
+++ helpers/atelierAPI.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+const config = require('../config.js');
+
+const host = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/';
+const header = {
+  'User-Agent': 'request',
+  'Authorization': `token ${config.TOKEN}`
+};
+
+var getDataByPath = (path) => {
+
+};
+
+module.exports = {
+
+};
