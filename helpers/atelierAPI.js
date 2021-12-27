a12989037cec3b7d187ae747dfc2584e7eb65bb0
Wisdom Ibole <wisdom.ibole@gmail.com>
2021-12-26 22:39:55 -0800

    Adds GET, POST, and PUT methods to be used in server.js to interact w API

--- helpers/atelierAPI.js
+++ helpers/atelierAPI.js
@@ -1,16 +1,41 @@
 const axios = require('axios');
 const config = require('../config.js');
 
-const host = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/';
-const header = {
+const host = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp';
+const headers = {
   'User-Agent': 'request',
   'Authorization': `token ${config.TOKEN}`
 };
 
 var getDataByPath = (path) => {
+  let options = {
+    url: host + path,
+    headers: headers
+  }
 
+  return axios.get(options.url, options);
+};
+
+var postDataByPath = (path, data) => {
+  let options = {
+    url: host + path,
+    headers: headers
+  }
+
+  return axios.post(options.url, data, options);
+};
+
+var putDataByPath = (path, data) => {
+  let options = {
+    url: host + path,
+    headers: headers
+  }
+
+  return axios.put(options.url, data, options);
 };
 
 module.exports = {
-
+  getDataByPath,
+  postDataByPath,
+  putDataByPath
 };
