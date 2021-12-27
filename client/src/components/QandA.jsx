683678921786461b94ce4eb94a5fe066d439c126
Wisdom Ibole <wisdom.ibole@gmail.com>
2021-12-26 20:43:38 -0800

    Installed express for use on server.js

---
+++ client/src/components/QandA.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './QandA/SearchBar.jsx';
+
+const QandA = (props) => {
+
+  return (
+    <div>
+      <p>Questions & Answers</p>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default QandA;
---
+++ client/src/components/QandA/QuestionsList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const QuestionsList = () => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default QuestionsList;
