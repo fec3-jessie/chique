b8ffaa6601aa3f240198a36cb554d4372b96c456
Kavita Poduri <kavi.pdri@gmail.com>
2021-12-27 18:05:23 -0800

    url and token import

--- client/src/components/Related/RelatedProducts.jsx
+++ client/src/components/Related/RelatedProducts.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import axios from 'axios';
+import {url, token} from '/config.js'
+
+function test () {
+};
 
 const RelatedProducts = (props) => {
+  console.log(url);
   return (<div>
     Related Products go here.
+    {}
   </div>)
 }
 
