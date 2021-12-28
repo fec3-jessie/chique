935d5437f130aae3d53615e245c148fc7895cec9
Wisdom Ibole <wisdom.ibole@gmail.com>
2021-12-27 19:31:21 -0800

    Changes time format to readable MM DD, YYYY

--- client/src/components/QandA/QuestionsList/Answers/AnswerDetails.jsx
+++ client/src/components/QandA/QuestionsList/Answers/AnswerDetails.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+const options = { month: 'long', day: 'numeric', year: 'numeric' };
+
 const AnswerDetails = ({ name, date, helpfulness, ID }) => {
+  const convertDate = new Date(date);
+  const newDate = convertDate.toLocaleDateString('en-US', options);
+
   return (
     <section>
-        by {name}, {date} | Helpful? <span>Yes ({helpfulness})</span> | <span>Report</span>
+        by {name}, {newDate} | Helpful? <span>Yes ({helpfulness})</span> | <span>Report</span>
     </section>
   );
 }
