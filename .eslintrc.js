0b090704430449a4f83e3fc2f717dfc15283dcbb
Kavita Poduri <kavi.pdri@gmail.com>
2021-12-27 18:26:21 -0800

    eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,23 @@
 module.exports = {
-  extends: 'hackreactor',
-  rules: {
-    // Rules here will override the 'hackreactor' configuration
-    // http://eslint.org/docs/rules/
-  }
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
