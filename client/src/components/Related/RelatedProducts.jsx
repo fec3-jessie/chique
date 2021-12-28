3ab3b02edf23b8f9c19803e5418a3ba53152afb5
Kavita Poduri <kavi.pdri@gmail.com>
2021-12-28 11:38:39 -0800

    successfully imported related products data

--- client/src/components/Related/RelatedProducts.jsx
+++ client/src/components/Related/RelatedProducts.jsx
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {url, token} from '/config.js'
-import 'regenerator-runtime';
 
 const RelatedProducts = () => {
 
+  const [products, setProducts] = useState([]);
+  const productId = '40344';
+  console.log(productId);
+
+  useEffect(() =>{
+    console.log('getting related products...')
+    axios.get(`${url}/products/${productId}/related`, {
+      headers: { 'Authorization': token }
+    })
+    .then(res => {
+      console.log(res.data);
+      axios.get(`${url}/products/`, {
+        headers: { 'Authorization': token }
+      })
+      // .then(products => console.log(products))
+      .then(products => products.data.filter(product => res.data.includes(product.id)))
+      .then(filteredProducts => setProducts(filteredProducts))
+    });
+  }, []);
+
   return (<div>
-    Related Products go here.
-    {}
+    <h3>Related Products</h3>
+    {products.length !== 0 ? products.map(product => product.id) : null}
   </div>)
 }
 
