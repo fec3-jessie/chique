f4633d51a03f382c0597bfb6ef61ffdb2f6a0570
Ryan Olea <ryanaolea@gmail.com>
2021-12-29 10:49:00 -0800

    updated styles to render sidebar in proper place

--- client/src/components/Ratings.jsx
+++ client/src/components/Ratings.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import Axios from 'axios';
-
-import Token from './token.jsx';
 import ReviewsSidebar from './RatingsComponents/ReviewsSidebar/ReviewsSidebar.jsx';
 import ReviewsList from '../components/RatingsComponents/ReviewsList/ReviewsList.jsx';
 import Token from './token.jsx';
@@ -11,7 +9,7 @@ function Ratings () {
   const [productReviews, setProductReviews] = useState({});
 
   useEffect(() => {
-    const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews?product_id=40359';
+    const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews?product_id=40358';
     const fetchReviews = async () => {
       const getReviews = await Axios.get(url, {
         headers: {
@@ -28,10 +26,11 @@ function Ratings () {
   return (
       <div className='reviews'>
         <h3>{`Ratings & Reviews`}</h3>
-        {productReviews.product !== undefined ?
-        <ReviewsSidebar productId={productReviews.product}/> : null}
-
-        <ReviewsList reviews={productReviews}/>
+        <div className='reviews-features'>
+          {productReviews.product !== undefined ?
+          <ReviewsSidebar className='reviews-sidebar' productId={productReviews.product}/> : null}
+          <ReviewsList className='reviews-list' reviews={productReviews}/>
+        </div>
       </div>
   )
 }
