1e59bf4f63e845cf106d6c8e2661a7200e80cca1
Kavita Poduri <kavi.pdri@gmail.com>
2021-12-29 11:58:26 -0800

    fixed config stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import Overview from './components/Overview.jsx'
 // import QandA from './components/QandA.jsx'
-// import Ratings from './components/Ratings.jsx'
+import Ratings from './components/Ratings.jsx'
 import Related from './components/Related.jsx'
 
 
@@ -16,9 +16,9 @@ class App extends React.Component {
         {/* <h1>Overview</h1>
         <Overview />
         <h1>QandA</h1>
-        <QandA />
+        <QandA /> */}
         <h1>Ratings</h1>
-        <Ratings /> */}
+        <Ratings />
         <h1>Related</h1>
         <Related />
       </div>
