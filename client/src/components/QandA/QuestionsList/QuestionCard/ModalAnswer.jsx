da9aa1918e653ea46257b0213b17a8696ee6b945
Wisdom Ibole <wisdom.ibole@gmail.com>
2021-12-28 23:37:37 -0800

    Completes preliminary implementation of Add Answer modal

--- client/src/components/QandA/QuestionsList/QuestionCard/ModalAnswer.jsx
+++ client/src/components/QandA/QuestionsList/QuestionCard/ModalAnswer.jsx
@@ -14,10 +14,42 @@ const ModalAnswer = ({ setShowModal }) => {
   return ReactDom.createPortal(
     <div className='container-modal' ref={modalRef} onClick={closeModal}>
       <div className='modal-answer'>
+        <h3>Submit your answer</h3>
+        <h4>Product Name:</h4>
         <form>
+          <label for='answer-text'>Your Answer:</label><br/>
+          <textarea
+            id='answer-text' maxlength='1000' required
+            rows='12' cols='50'
+          ></textarea>
+          {/* <input type='text' id='answer-text' maxlength='1000' required/> */}
 
+          <br/>
+          <br/>
+
+          <label for='answer-nickname'>What is your nickname?</label><br/>
+          <input type='text' id='answer-nickname' maxlength='60'
+            placeholder='Example: jack543!' size='30' required />
+          <p id='answer-disclaimer'>For privacy reasons, do not use your full name or email address</p>
+
+          <br/>
+
+          <label for='answer-email'>Your email:</label><br/>
+          <input type='email' id='answer-email' maxlength='60'
+            placeholder='jack@email.com' size='30' required />
+          <p id='answer-disclaimer'>For authentication reasons, you will not be emailed</p>
+
+          <br/>
+
+          <label for='answer-photos'>Upload your photos (5 Max):</label><br/>
+          <input type='file' id='answer-photos' accept='image/*' />
+
+          <br/>
+          <br/>
+
+          <input type='submit' value='Submit answer' id='answer-submit' />
         </form>
-        <h2>This is a Modal</h2>
+
         <button onClick={() => setShowModal(false)}>X</button>
       </div>
     </div>,
