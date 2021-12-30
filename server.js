be968aebd920e3f5e143b0c0927527279dd2bc6d
Wisdom Ibole <wisdom.ibole@gmail.com>
2021-12-29 16:40:38 -0800

    Comments out all contents of server.js; not currently used

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 3000;
-const atelierAPI = require('./helpers/atelierAPI.js');
+// const express = require('express');
+// const path = require('path');
+// const PORT = process.env.PORT || 3000;
+// const atelierAPI = require('./helpers/atelierAPI.js');
 
-const app = express();
+// const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'client/dist')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, '..', 'client/dist')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 // app.listen(PORT, () => {
 //   console.log(`Listening on port ${PORT}...`)
