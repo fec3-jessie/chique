e9bd873e244a8c8341d7353b49921225f5e65069
Wisdom Ibole <wisdom.ibole@gmail.com>
2021-12-30 15:29:54 -0800

    Establishes initial connection on page load using Product 40344

---
+++ server.js
@@ -0,0 +1,18 @@
+// import express from 'express';
+// import { token } from './config.js';
+// const router = require('express').Router();
+
+// const app = express();
+
+// const headers = {
+//   'Authorization': token
+// };
+
+// // Middleware
+
+// // Routing
+
+
+// app.listen(3000, () => {
+//   console.log('Server listening on port 3000...')
+// });
