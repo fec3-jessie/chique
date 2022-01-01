1ab030099372a0566fffb3ca5e3865ff9656b2e7
Wisdom Ibole <wisdom.ibole@gmail.com>
2021-12-31 22:08:55 -0800

    Adds asterisk to visually indicate required inputs on modal

--- client/src/components/QandA/QuestionsList/QuestionCard/Modal.jsx
+++ client/src/components/QandA/QuestionsList/QuestionCard/Modal.jsx
@@ -30,24 +30,23 @@ const Modal = ({ setShowModal, usage, product_name }) => {
         <h3>{title}</h3>
         <h4>{subtitle}</h4>
         <form>
-          <label htmlFor='answer-text'>{title_body}</label><br/>
+          <label htmlFor='answer-text'>{title_body}<span id='required-input'>*</span></label><br/>
           <textarea
             id='answer-text' maxLength='1000' required
             rows='12' cols='50'
           ></textarea>
-          {/* <input type='text' id='answer-text' maxlength='1000' required/> */}
 
           <br/>
           <br/>
 
-          <label htmlFor='answer-nickname'>What is your nickname?</label><br/>
+          <label htmlFor='answer-nickname'>What is your nickname?<span id='required-input'>*</span></label><br/>
           <input type='text' id='answer-nickname' maxLength='60'
             placeholder='Example: jack543!' size='30' required />
           <p id='answer-disclaimer'>For privacy reasons, do not use your full name or email address</p>
 
           <br/>
 
-          <label htmlFor='answer-email'>Your email:</label><br/>
+          <label htmlFor='answer-email'>Your email:<span id='required-input'>*</span></label><br/>
           <input type='email' id='answer-email' maxLength='60'
             placeholder='jack@email.com' size='30' required />
           <p id='answer-disclaimer'>For authentication reasons, you will not be emailed</p>
