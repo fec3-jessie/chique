96a81d23dbcc9ecefe9833b836d68474d2d25c1d
Wisdom Ibole <wisdom.ibole@gmail.com>
2022-01-02 13:22:05 -0800

    Removes commented out lines.

--- client/src/components/QandA/QuestionsList/HelpfulTracker.jsx
+++ client/src/components/QandA/QuestionsList/HelpfulTracker.jsx
@@ -1,17 +1,6 @@
 import React, { useState } from 'react';
-// import { token, url } from '/config.js';
 import axios from 'axios';
 const localHost = 'http://127.0.0.1:3000';
-
-// class HelpfulTracker extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       yesScore: this.props.helpful,
-//       yesClicked: false
-//     }
-//   }
-// }
 
 const HelpfulTracker = ({ question_id, helpful, usage, reported }) => {
   const [yesCount, setYesCount] = useState(helpful);
@@ -20,16 +9,14 @@ const HelpfulTracker = ({ question_id, helpful, usage, reported }) => {
   const [reportClicked, setReportClicked] = useState(reported);
 
   const updateYesOrReport = (endpoint, body) => {
-    // axios.put(url + endpoint, body, { headers: { 'Authorization': token }})
     axios.put(localHost + endpoint, body)
       .catch(err => console.error('Error submitting PUT req (HelpfulTracker.jsx): ', err))
   };
 
   const onYesClick = () => {
-    let endpoint, body;
+    let endpoint;
+    let body;
     if (yesClicked === false) {
-      // setYesCount(prevYesCount => prevYesCount + 1);
-      // setYesClicked(true);
       if (usage === 'answer') {
         endpoint = `/qa/answers/${question_id}/helpful`;
         body = { helpfulness: yesCount + 1 };
@@ -44,10 +31,9 @@ const HelpfulTracker = ({ question_id, helpful, usage, reported }) => {
   };
 
   const onReportClick = () => {
-    let endpoint, body;
+    let endpoint;
+    let body;
     if (reportClicked === false) {
-      // setReportState('Reported');
-      // setReportClicked(true);
       if (usage === 'question') {
         endpoint = `/qa/questions/${question_id}/report`;
         body = { reported: 'Reported' };
