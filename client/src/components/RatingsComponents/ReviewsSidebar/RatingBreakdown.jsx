5419b6ed320aa5f5cd4bc923858a9d6811f8899c
Ryan Olea <ryanaolea@gmail.com>
2022-01-01 19:49:16 -0800

    implemented starsLabel click handler to change style state onClick/ need to connect to filter state

--- client/src/components/RatingsComponents/ReviewsSidebar/RatingBreakdown.jsx
+++ client/src/components/RatingsComponents/ReviewsSidebar/RatingBreakdown.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import StarBar from './StarBar.jsx';
 
 
 function RatingBreakdown ({ratings}) {
   let starsCounts = ['1', '2', '3', '4', '5'];
+  const [starsClicked, setStarsClicked] = useState({
+    '1': false,
+    '2': false,
+    '3': false,
+    '4': false,
+    '5': false
+  });
+  const starClickedStyles = {
+    'text-decoration': 'underline'
+  };
+  const starUnClickStyles = {
+    'text-decoration': 'none'
+  };
+
   return (
     <div className='stars-breakdown-container'>
       {starsCounts.map((star) => {
         return (
           <div key={star} className='stars-bar-container'>
-            {/* <p className='stars-bar-label' key={star}>{`${star}`}</p>
-            <p className='stars-bar-label'>{`stars`}</p> */}
-            <a className='stars-bar-label' key={star}>{`${star}`}<span className='star-span'>stars</span></a>
+            <a className='stars-bar-label'
+              onClick={(e) => {
+                setStarsClicked(prevState => ({
+                  ...prevState,
+                  [star] : !prevState[star]
+                }));
+              }}
+              key={star}>{`${star}`}
+              <span className='star-span'
+                style={starsClicked[star] === true ? starClickedStyles : starUnClickStyles}>
+                stars
+              </span>
+            </a>
             <StarBar className='stars-bar' ratings={ratings} key={-star} star={star}/>
           </div>
         )
