b428fa85b32664ac960baea5a1218c98ba8e3303
Ryan Olea <ryanaolea@gmail.com>
2022-01-05 08:58:19 -0800

    stubbed out helper funcs for click listening

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,25 @@ import Ratings from './components/Ratings.jsx';
 import Related from './components/Related.jsx';
 import QandA from './components/QandA.jsx';
 import axios from 'axios';
-import { token, url } from '/config.js';
 
-const headers = {
-  'Authorization': token
+const findId = (node) => {
+  let id;
+  let moduleIds = {
+    'Overview': true,
+    'Related': true,
+    'QandA': true,
+    'Ratings': true,
+  };
+  //base case//
+    // id can be found in moduleIds
+      // set id to this id
+      //return from recursion
+    // else recursion
+  // do some stuff to recurse the dom tree//
+    // if parentNodes check ids if false continue
+  return id;
 };
-// 40349
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -35,26 +48,46 @@ class App extends React.Component {
     cb(itemId);
   }
 
+
+
   render () {
+
+    window.addEventListener('click', function (e) {
+      let body = {
+        time: e.timeStamp, // convert to string // what format?
+        module: e.target.parentElement.childNodes[e.target.parentElement.childNodes.length - 1], // use findId func here
+        // parentElementClass: e.srcElement.parentElement.className,
+        // parentElementTag: e.srcElement.parentElement.nodeName
+        element: `${e.srcElement.parentElement.nodeName}-${e.srcElement.parentElement.className}`
+      };
+      console.log('this is your click event', body);
+
+      // create axios post to /interactions here
+    });
+
     return (
       <div>
         <h1>Overview</h1>
         <Overview
+          id='Overview'
           product_Id={this.state.product_Id}
           handleProductNameChange={this.handleProductNameChange}
         />
         <h1>Related</h1>
         <Related
+          id='Related'
           product_Id={this.state.product_Id}
           relatedClickHandler={this.relatedClickHandler}
         />
         <h1>QandA</h1>
         <QandA
+          id='QandA'
           product_id={this.state.product_Id}
           product_name={this.state.product_name}
         />
         <h1>Ratings</h1>
         <Ratings
+          id='Ratings'
           product_Id={this.state.product_Id}
           productName={this.state.product_name}
         />
