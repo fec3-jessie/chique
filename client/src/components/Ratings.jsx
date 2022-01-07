047be54a6b612db0e9bb4cebc30bb84a2a090e1a
Wisdom Ibole <wisdom.ibole@gmail.com>
2022-01-06 17:24:11 -0800

    Removes all references to 127.0.0.1:3000

--- client/src/components/Ratings.jsx
+++ client/src/components/Ratings.jsx
@@ -21,7 +21,7 @@ function Ratings ({product_Id, productName}) {
 
 
   useEffect(() => {
-    const url = `http://127.0.0.1:3000/reviews`;
+    const url = `/reviews`;
     const params = {product_id: product_Id, count: 50, sort: sort};
     const fetchReviews = async () => {
       const getReviews = await Axios.get(url, {
@@ -38,7 +38,7 @@ function Ratings ({product_Id, productName}) {
   const [factors, setFactors] = useState([]);
 
   useEffect(() => {
-    const url = 'http://127.0.0.1:3000/reviews/meta';
+    const url = '/reviews/meta';
     const params = {product_id: product_Id};
 
     const fetchMeta = async () => {
