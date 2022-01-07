304278f598e5ab56f772060965156ac989aec216
Daniel-Ghaly <ghalydan@gmail.com>
2022-01-07 14:59:19 -0800

    made changes

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -108,7 +108,7 @@ class Overview extends React.Component {
   }
 
   componentWillReceiveProps() {
-    axios.get('http://127.0.0.1:3000/reviews', { params: { product_id: this.props.product_Id }})
+    axios.get(`${localhost}/reviews`, { params: { product_id: this.props.product_Id }})
       .then(res => {
         this.setState({numberOfReviews: res.data.results.length});
 
@@ -120,7 +120,7 @@ class Overview extends React.Component {
         this.setState({rating: rating});
       });
 
-    axios.get(`http://127.0.0.1:3000/products/${this.props.product_Id}`)
+    axios.get(`${localhost}/products/${this.props.product_Id}`)
       .then(res => {
         this.setState({productName: res.data.name});
         this.setState({productCategory: res.data.category});
@@ -130,7 +130,7 @@ class Overview extends React.Component {
 
       });
 
-    axios.get(`http://127.0.0.1:3000/products/${this.props.product_Id}/styles`)
+    axios.get(`${localhost}/products/${this.props.product_Id}/styles`)
       .then(res => {
         // maintain styles API results in state, so that I can use it later in lifecycle
         this.setState({results: res.data.results});
