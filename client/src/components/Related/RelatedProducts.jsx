d77ae17b7c3a11b8416c10664ae45a81223b0c4c
Wisdom Ibole <wisdom.ibole@gmail.com>
2022-01-06 16:43:32 -0800

    Catches all and makes use of server.js

--- client/src/components/Related/RelatedProducts.jsx
+++ client/src/components/Related/RelatedProducts.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {token} from '/config.js';
 import ProductCard from './ProductCard.jsx';
 const url = 'http://localhost:3000';
 
